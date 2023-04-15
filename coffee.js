914ab57590efd3b17e1e410791104502a1ff32b1
Sushma911 <130823149+Sushma911@users.noreply.github.com>
2023-04-15 17:36:36 +0530

    practice coffee
    practice

---
+++ coffee.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('#icon').click(function(){
+        $('ul').toggleClass('show');
+    });
+
+
+});
+
+AOS.init();
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("p").toggle();
+//     });
+// });
+
+
+// function myFunction(){
+//   alert("i m a alert box");
+// }
